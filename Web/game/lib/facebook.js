90e7196044c85fe4f4fca776668212364b3bdbe5
Kim-Dae woon <hjchokj@naver.com>
2017-09-27 19:38:41 +0900

    add license

--- Web/game/lib/facebook.js
+++ Web/game/lib/facebook.js
@@ -1,6 +1,25 @@
+/**
+Rule the words! KKuTu Online
+Copyright (C) 2017 JJoriping(op@jjo.kr)
+Copyright (C) 2017 KKuTu Korea(op@kkutu.co.kr)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 window.fbAsyncInit = function() {
 	FB.init({
-		appId      : '635305196655663',
+		appId      : '<APPID>',
 		xfbml      : true,
 		version    : 'v2.8'
 	});
@@ -9,6 +28,6 @@ window.fbAsyncInit = function() {
 	var js, fjs = d.getElementsByTagName(s)[0];
 	if (d.getElementById(id)) {return;}
 	js = d.createElement(s); js.id = id;
-	js.src = "//connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v2.5&appId=635305196655663";
+	js.src = "//connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v2.5&appId=<APPID>";
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
